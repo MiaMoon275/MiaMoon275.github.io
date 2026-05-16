83d394d8a87a6f30dc7ae481c0b4a0461bd6202d
MiaMoon275 <160272131+MiaMoon275@users.noreply.github.com>
2026-05-16 19:41:29 +0200

    Initial portfolio site with GitHub API
    * Layout & Design using Bootstrap and CSS adjustments
    * Fetching user data from github API

---
+++ js/github-api.js
@@ -0,0 +1,94 @@
+const username = 'MiaMoon275'; 
+const url = `https://api.github.com/users/${username}`;
+
+const toolsNames = [
+    // Backend / Programming
+    "python",
+    "django",
+
+    // Core Web
+    "html5",
+    "css3",
+    "javascript",
+
+    // UI / Framework
+    "bootstrap",
+
+    // Version Control / Workflow
+    "git",
+
+    // IDEs
+    "vscode",
+    "visualstudio",
+    "intellij",
+
+    // Game Dev
+    "csharp",
+    "unity",
+];
+
+function loadData() {
+    const cached = localStorage.getItem("github-data");
+    if (cached){
+        const data = JSON.parse(cached)
+        console.log("Data loaded from cache:")
+        console.log(data);
+        applyData(data);
+        return;
+    }
+
+    console.log("Fetching data from github")
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            localStorage.setItem("github-data", JSON.stringify(data));
+            applyData(data);
+        });
+    }
+
+function applyData(data) {
+    // Personal Data
+    document.getElementById('github-name').textContent = data.name || data.login;
+    document.getElementById('github-avatar').src = data.avatar_url;
+
+    // Other
+    document.getElementById('github-company').textContent = data.company;
+    document.getElementById('github-location').textContent = data.location;
+}
+
+function loadTools() {
+    const container = document.getElementById("familiar-tools");
+
+    const cached = localStorage.getItem("tool-icons");
+
+    if (cached) {
+        console.log("Loaded Tools from cache.")
+        container.innerHTML = cached;
+        return;
+    }
+
+    let html = "";
+
+    toolsNames.forEach(tool => {
+        const baseUrl = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${tool}/`;
+
+        html += `
+            <span class="badge bg-dark m-1 d-inline-flex align-items-center gap-1 p-2">
+                <img src="${baseUrl}${tool}-original.svg"
+                     alt="${tool}"
+                     width="24"
+                     height="24"
+                     onerror="this.onerror=null; this.src='${baseUrl}${tool}-plain.svg'">
+                ${tool}
+            </span>
+        `;
+    });
+
+    container.innerHTML = html;
+    localStorage.setItem("tool-icons", html);
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    loadData();
+    loadTools();
+})
---
+++ js/github-repo-loader.js
@@ -0,0 +1,1 @@
+const REPO_URL = "https://api.github.com/users/MiaMoon275/repos"
